80969a38d24209eafc26e6178c3da2c967362bfa
Esther Muigai <esthermuigaimuthoni@gmail.com>
2019-02-18 15:04:29 +0300

    This commit will customize the control flows

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
-function splitString() {
+//USER INTERFACE LOGIC
+function splitString(stringToSplit, separator) {
+  return myTriangle= parseInt(stringToSplit.split(separator));
+}
+  
+  //BUSINESS LOGIC
+function triangleTracker(){
   var triangleSides = document.getElementById("tSides").value;
   var comma = ",";
-var myTriangle = triangleSides.split(comma);
-}
-
+  splitString (triangleSides,comma);
+  if ((myTriangle[0] + myTriangle[1] <= myTriangle[2]) || (myTriangle[1] + myTriangle[2] <= myTriangle[0]) || (myTriangle[2] + myTriangle[0] <= myTriangle[1])) {
+    alert('This is not a triangle.');
+  } else if ((myTriangle[0] === myTriangle[1]) && (myTriangle[1] === myTriangle[2]) && (myTriangle[0] === myTriangle[2])) {
+    alert('This is an equilateral triangle. All sides are equal');
+  } else if ((myTriangle[0] === myTriangle[1]) || (myTriangle[1] === myTriangle[2]) || (myTriangle[0] === myTriangle[2])) {
+    alert('This is an isosceles triangle. Two sides are equal.');
+  } else if ((myTriangle[0] + myTriangle[1] > myTriangle[2]) || (myTriangle[1] + myTriangle[2] > myTriangle[0]) || myTriangle[2] + myTriangle[0] > myTriangle[1]){
+  	 alert('This is a scalene triangle.');
+  }
+};
+  
