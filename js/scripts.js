9f6bf8fb89aa9437d8ea7b601e2b99c95c535d7c
Esther Muigai <esthermuigaimuthoni@gmail.com>
2019-02-18 13:06:56 +0300

    This commit will link the html input to the js function

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,6 @@
+function splitString() {
+  var triangleSides = document.getElementById("tSides").value;
+  var comma = ",";
+var myTriangle = triangleSides.split(comma);
+}
 
-var a = 9;
-var b = 4;
-var c = 3;
-
-if (a + b <= c || b + c <= a || c + a <= b) {
-   alert('This is not a triangle. The sum of two sides is equal to or less than the third side')
-   } else if (a === b && b === c && a === c) {
-    alert('This is an equilateral triangle. All sides are equal');
-   } else if (a === b || b === c || a === c) {
-   alert('This is an isosceles triangle. Two sides are equal.');
-   } else if (a + b > c || b + c > a || c + a > b) {
-   alert('This is a scalene triangle. The sum of two sides is greater than the third side.');
-   }
